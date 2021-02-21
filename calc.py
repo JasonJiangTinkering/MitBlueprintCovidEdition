a527a8d8034b006bb55f4b76ac069a56a6da844a
Steven Rakhmanchik <48323187+stevenrakhmanchik@users.noreply.github.com>
2021-02-21 12:58:59 -0500

    Add files via upload

--- calc.py
+++ calc.py
@@ -10,29 +10,33 @@ import numpy as np
 
 datapath = 'data/'
 
-def brightness(im_file):
-	im = Image.open(im_file)
-	stat = ImageStat.Stat(im)
-	r, g, b = stat.mean
-	math.sqrt(0.241*(r**2) + 0.691*(g**2) + 0.068*(b**2))
-
-def do(newdict,rounds,info,names):
+def do(newdict,info,names):
     new = []
-    for xz in info:
-    	y = xz[3]
-    	c = xz[2]
-    	helperlist = xz[4]
+    for i in range(0, len(info)):
+    	xz = info[i]
+    	name = names[i]
+    	helperlist = xz[2]
     	im = newdict[xz[0]]
+    	c = 0
+    	a = 0
     	im = im.convert('RGB')
     	im.save('data/test.jpg')
-    	a = 0
     	stat = ImageStat.Stat(im)
     	r,g,b = stat.mean
     	x = math.sqrt(0.241*(r**2) + 0.691*(g**2) + 0.068*(b**2))
+    	if len(helperlist) > 20:
+    		del helperlist[0]
     	helperlist.append(x)
-    	if abs(x - y)>2:
-    		c+=1
-    	std = statistics.stdev(helperlist)
-    	new.append([xz[0],a,c,x,helperlist,std])
-    return(new) # new = [ NAME , 0 , 
-
+    	hl2 = [j-i for i, j in zip(helperlist[:-1], helperlist[1:])]
+    	for x in hl2:
+    		if x > 2:
+    			c=c+1
+    	y = helperlist[len(helperlist)-1]
+    	#if len(hl2) > 1:
+    	#	std = statistics.stdev(hl2)
+    	#else:
+    	#	std = 0
+    	#if std > 6:
+    	#	print(name + ' is probably cheating')
+    	new.append([xz[0],a,helperlist,c])
+    return(new)

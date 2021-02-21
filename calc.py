ec8cd036637fd184c04322c371d8d089518d8e53
Steven Rakhmanchik <48323187+stevenrakhmanchik@users.noreply.github.com>
2021-02-21 13:07:08 -0500

    Add files via upload

--- calc.py
+++ calc.py
@@ -29,7 +29,7 @@ def do(newdict,info,names):
     	helperlist.append(x)
     	hl2 = [j-i for i, j in zip(helperlist[:-1], helperlist[1:])]
     	for x in hl2:
-    		if abs(x) > 2:
+    		if abs(x) > 4:
     			c=c+1
     	y = helperlist[len(helperlist)-1]
     	#if len(hl2) > 1:
